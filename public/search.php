37a870fc85b9d26ab308b30e340d6a17e305eb92
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2024-04-09 15:35:35 -0400

    Fixes for deployment

--- public/search.php
+++ public/search.php
@@ -1,13 +1,11 @@
+#!/usr/local/bin/php
 <?php
 require '../vendor/autoload.php';
 // get the search term from the form
 $searchTerm = $_POST['searchTerm'];
-// include the Database class
-//require_once '../src/Database.php';
-// include the Game class
-require_once '../src/entities/Game.php';
+use Tabletop\Entities\Game;
 // get the games that match the search term
-$games = Tabletop\Entities\Game::searchGamesByName($searchTerm);
+$games = Game::searchGamesByName($searchTerm);
 foreach ($games as $game) {
     echo "<h3><a href='game/info.php?game_id={$game->getId()}'>{$game->getName()}</a></h3>";
     echo "<p>Players: {$game->getMinPlayers()} - {$game->getMaxPlayers()}</p>";
