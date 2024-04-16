826970ee2e2b6a0297272ccbdb643778592a09b9
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2024-04-16 14:44:02 -0400

    Description and image added to game page

--- public/game/info.php
+++ public/game/info.php
@@ -10,6 +10,12 @@ if (!isset($game_id)) {
     if (!$game) {
         $error = "Game not found.";
     }
+    $apiResponse = file_get_contents("https://boardgamegeek.com/xmlapi2/thing?id=$game_id");
+    if (!$apiResponse) {
+        $error = "Game information not found.";
+    }
+    $imageURL = simplexml_load_string($apiResponse)->item->image;
+    $description = simplexml_load_string($apiResponse)->item->description;
 }
 ?>
 <!DOCTYPE html>
@@ -21,25 +27,14 @@ if (!isset($game_id)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 
 <body>
-<header class="container-fluid bg-dark text-white m-0">
-    <h1 class="text-center py-5 mb-0">Tabletop Tavern</h1>
-</header>
-<nav class="navbar navbar-expand-lg bg-dark bg-opacity-75 navbar-dark py-3 justify-content-center">
-    <ul class="navbar-nav justify-content-around w-75">
-        <li class="nav-item"><a class="nav-link active" href="../index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="../about.php">About</a></li>
-        <li class="nav-item"><a class="nav-link" href="../filter.php">Filter Search</a></li>
-        <li class="nav-item"><a class="nav-link" href="../creators.php">Creators</a></li>
-        <?php if (isset($_SESSION['user'])): ?>
-            <li class="nav-item"><a class="nav-link" href="../logout.php">Logout <?php echo $_SESSION['username'] ?></a></li>
-        <?php else: ?>
-            <li class="nav-item"><a class="nav-link" href="../login.php">Login</a></li>
-        <?php endif; ?>
-    </ul>
-</nav>
+<?php include '../header.php';?>
 
 <div class="container mt-3">
 <?php if (!isset($game)): ?>
@@ -55,9 +50,12 @@ if (!isset($game_id)) {
     <p>Minimum Age: <?= $game->getMinAge() ?></p>
     <p>Rating: <?= $game->getRatingAverage() ?> (<?= $game->getRatingCount() ?> votes)</p>
     <p>Year Published: <?= $game->getYearPublished() ?></p>
+    <img src="<?= $imageURL ?>" alt="<?= $game->getName() ?>" class="img-fluid">
+    <p><?= $description ?></p>
 <?php endif; ?>
 </div>
 
+<?php include '../footer.php';?>
 </body>
 
 </html>
