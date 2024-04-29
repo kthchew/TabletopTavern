005f94ca77e2969576a7183e1ff102b95e148533
VallinEMCSQ <vallinc@ufl.edu>
2024-04-28 23:10:56 -0400

    Filter search working

--- public/filter.php
+++ public/filter.php
@@ -1,7 +1,33 @@
 #!/usr/local/bin/php
 <?php
 session_start();
+require '../vendor/autoload.php';
+use Tabletop\Entities\Game;
 define('__HEADER_FOOTER_PHP__', true);
+
+$gameCards = '';
+
+//Receive the form data and call the searchGamesByMultipleParameters function from the Game class
+if (isset($_POST['searchName']) || isset($_POST['searchGenre']) || isset($_POST['minPlayers']) || isset($_POST['maxPlayers']) || isset($_POST['playTime']) || isset($_POST['minAge'])) {
+    $searchName = $_POST['searchName'];
+    $searchGenre = $_POST['searchGenre'];
+    $minPlayers = $_POST['minPlayers'];
+    $maxPlayers = $_POST['maxPlayers'];
+    $playTime = $_POST['playTime'];
+    $minAge = $_POST['minAge'];
+    $pageSize = 10;
+    $currentPage = 1;
+
+    //var_dump($searchName, $searchGenre, $minPlayers, $maxPlayers, $playTime, $minAge, $pageSize, $currentPage);
+
+    $games = Game::searchGameByMultipleParameters( $searchName, $minPlayers, $maxPlayers, $playTime, $minAge, $pageSize, $currentPage);
+
+    foreach ($games as $game) {
+        $gameCards .= $game->cardView();
+    }
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +44,20 @@ define('__HEADER_FOOTER_PHP__', true);
 
 <body>
 <?php include 'header.php';?>
+
+<form action="" method="post">
+    <input type="text" name="searchName" placeholder="Search by Name...">
+    <input type="text" name="searchGenre" placeholder="Search by Genre...">
+    <input type="number" name="minPlayers" placeholder="Minimum Players...">
+    <input type="number" name="maxPlayers" placeholder="Maximum Players...">
+    <input type="number" name="playTime" placeholder="Play Time...">
+    <input type="number" name="minAge" placeholder="Minimum Age...">
+    <input type="submit" value="Search">
+</form>
+
+<div class="row row-cols-4 mb-4">
+    <?php echo $gameCards; ?>
+</div>
 
 <?php include 'footer.php';?>
 </body>
--- src/Entities/Game.php
+++ src/Entities/Game.php
@@ -91,6 +91,65 @@ class Game
         return $games;
     }
 
+    static function searchGameByMultipleParameters($name, $minPlayers, $maxPlayers, $playTime, $minAge, $pageSize, $currentPage): array
+    {
+        $db = Database::getInstance();
+
+        // Initialize the query and the parameters array
+        $query = "SELECT * FROM Games WHERE ";
+        $params = [];
+        $types = "";
+
+        // Add conditions to the query based on the parameters that are not empty
+        if (!empty($name)) {
+            $query .= "name LIKE ? AND ";
+            $params[] = "%" . $name . "%";
+            $types .= "s";
+        }
+        if (!empty($minPlayers)) {
+            $query .= "min_players >= ? AND ";
+            $params[] = $minPlayers;
+            $types .= "i";
+        }
+        if (!empty($maxPlayers)) {
+            $query .= "max_players <= ? AND ";
+            $params[] = $maxPlayers;
+            $types .= "i";
+        }
+        if (!empty($playTime)) {
+            $query .= "play_time <= ? AND ";
+            $params[] = $playTime;
+            $types .= "i";
+        }
+        if (!empty($minAge)) {
+            $query .= "min_age <= ? AND ";
+            $params[] = $minAge;
+            $types .= "i";
+        }
+
+        // Remove the last "AND "
+        $query = substr($query, 0, -4);
+
+        // Add the order by, limit and offset clauses
+        $query .= " ORDER BY name LIMIT ? OFFSET ?";
+        $params[] = $pageSize;
+        $params[] = ($currentPage - 1) * $pageSize;
+        $types .= "ii";
+
+        // Prepare and execute the statement
+        $stmt = $db->prepare($query);
+        $stmt->bind_param($types, ...$params);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Fetch the games
+        $games = [];
+        while ($row = $result->fetch_assoc()) {
+            $games[] = self::makeGameFromDBRow($row);
+        }
+        return $games;
+    }
+
     static function searchGamesByGenre($subgenre): array
     {
         $db = Database::getInstance();
