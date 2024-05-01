4a5d43481cbc13bd95a1fc59979dce39620556e3
jonana-split <jmijares03@hotmail.com>
2024-04-30 21:32:06 -0400

    edited about page

--- public/about.php
+++ public/about.php
@@ -14,33 +14,40 @@ define('__HEADER_FOOTER_PHP__', true);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        .flex-container{
+            display: flex;
+        }
+    </style>
 </head>
 
 <body>
 <?php include 'header.php';?>
 <div class="container">
-    <div class="row align-items-center">
-        <div class="col text-end" >
-            <img src="images/tempLogo.png" alt="Left Logo" style="max-width: 50px; height: auto;">
-        </div>
-        <div class="col text-center">
-            <h1 style="white-space: nowrap;">Start your journey with</h1>
-        </div>
-        <div class="col text-start">
-            <img src="images/tempLogo.png" alt="Right Logo" style="max-width: 50px; height: auto;">
-        </div>
-    </div>
-    <div class="row mt-2">
-        <div class="col text-center">
-            <h1 style="margin-bottom: 0;">Tabletop Tavern!</h1>
+    <div flex-container>
+        <div class="row align-items-center">
+            <div class="col text-end" >
+                <img src="images/tempLogo.png" alt="Left Logo" style="max-width: 70px; height: auto;">
+            </div>
+            <div class="col text-center">
+                <h1 style="white-space: nowrap;">Start your journey with</h1>
+                <h1 style="margin-bottom: 0;">Tabletop Tavern!</h1>
+            </div>
+            <div class="col text-start">
+                <img src="images/tempLogo.png" alt="Right Logo" style="max-width: 70px; height: auto;">
+            </div>
         </div>
     </div>
+
     <div class="row mt-4">
-        <div class="col text-center">
-            <h2 style="font-size: 24px;">Greetings, tabletop game enthusiasts</h2>
-            <p style="font-size: 20px;" >Have you been looking for your next game to play? Or maybe you’re new to board games and want to find the perfect game for you. Find what you need here at the Tabletop Tavern!</p>
-            <p style="font-size: 20px;" >Board games and tabletop games can be hard to get into and difficult to find... There’s just so many options out there! At the Tabletop Tavern you can filter a large database of games by genre, number of players, age rating, mechanics, and more!</p>
-            <p style="font-size: 20px;" >Not sure what to filter by? Not a problem! Take our quiz to get a game suggested to you, or even “roll the dice” and find a completely random recommendation!</p>
+        <div class="col text-center" style = "padding-left: 100px; padding-right: 100px">
+            <br>
+            <h2 style="font-size: 24px;">Greetings, tabletop game enthusiasts!</h2>
+            <br>
+            <p style="font-size: 18px;" >Have you been looking for your next game to play? Or maybe you’re new to board games and want to find the perfect game for you. Find what you need here at the Tabletop Tavern!</p>
+            <p style="font-size: 18px;" >Board games and tabletop games can be hard to get into and difficult to find... There’s just so many options out there! At the Tabletop Tavern you can filter a large database of games by genre, number of players, age rating, mechanics, and more!</p>
+            <p style="font-size: 18px;" >Not sure what to filter by? Not a problem! Take our quiz to get a game suggested to you, or even “roll the dice” and find a completely random recommendation!</p>
             <br>
             <hr class="bold-divider">
             <h1 style="margin-bottom: 0;">Created By...</h1>
